4a1d644d87d4c74108851e8aa12aa554ce0b209f
puputcho <67434177+puputcho@users.noreply.github.com>
2023-01-18 17:16:06 -0300

    Add files via upload

--- main.js
+++ main.js
@@ -1,24 +1,23 @@
 x = '';
 y = '';
-circle = '';
+circ = '';
 retangle = '';
 reconhecerFala = window.webkitSpeechRecognition;
 reconhecer = new reconhecerFala();
 function start(){
     document.getElementById('status').innerHTML = 'Iniciando Reconhecimento <br> Fale "círculo" ou "retângulo" <br><br>'
-    reconhecer.start()
+    reconhecer.start();
     
 }
 reconhecer.onresult = function (event) {
     console.log(event);
-    var resultado = event.results[0][0].transcript;
-    console.log(results);   
+    var resultado = event.results[0][0].transcript;  
     document.getElementById('status').innerHTML = 'Comando identificado: ' + resultado;
     x = Math.floor(Math.random());
     y = Math.floor(Math.random());
     if (resultado=='círculo') {
         document.getElementById('status').innerHTML = 'Desenhando: "' + resultado + '"...';
-        circle = 'true';
+        circ = 'true';
     }
     if (resultado=='retângulo') {
         document.getElementById('status').innerHTML = 'Desenhando: "' + resultado + '"...';
@@ -30,11 +29,11 @@ function setup() {
     canvas.center();
 }
 function draw() {
-    if (circle == 'true') {
+    if (circ == 'true') {
         radius = Math.floor(Math.random());
         circle(x, y, radius);
         document.getElementById('status').innerHTML = 'A missão está completa';
-        circle = ''
+        circ = ''
     }
     if (retangle == 'true') {
         height = Math.floor(Math.random());
